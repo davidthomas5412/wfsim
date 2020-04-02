9744dc2bc0414055555245ae0b8c407c0bc7f694
Josh Meyers <jmeyers314@gmail.com>
2020-04-01 21:27:12 -0700

    Add starSim

---
+++ starSim.py
@@ -0,0 +1,409 @@
+import multiprocessing
+import numpy as np
+from scipy.optimize import bisect
+from tqdm import tqdm
+
+import galsim
+import batoid
+
+from wfTel import LSSTFactory
+
+wavelength_dict = dict(
+    u=365.49,
+    g=480.03,
+    r=622.20,
+    i=754.06,
+    z=868.21,
+    y=991.66
+)
+
+def BBSED(T):
+    """(unnormalized) Blackbody SED for temperature T in Kelvin.
+    """
+    waves_nm = np.arange(330.0, 1120.0, 10.0)
+    def planck(t, w):
+        # t in K
+        # w in m
+        c = 2.99792458e8  # speed of light in m/s
+        kB = 1.3806488e-23  # Boltzmann's constant J per Kelvin
+        h = 6.62607015e-34  # Planck's constant in J s
+        return w**(-5) / (np.exp(h*c/(w*kB*t))-1)
+    flambda = planck(T, waves_nm*1e-9)
+    return galsim.SED(
+        galsim.LookupTable(waves_nm, flambda),
+        wave_type='nm',
+        flux_type='flambda'
+    )
+
+
+def _vkSeeing(r0_500, wavelength, L0):
+    # von Karman profile FWHM from Tokovinin fitting formula
+    kolm_seeing = galsim.Kolmogorov(r0_500=r0_500, lam=wavelength).fwhm
+    r0 = r0_500 * (wavelength/500)**1.2
+    arg = 1. - 2.183*(r0/L0)**0.356
+    factor = np.sqrt(arg) if arg > 0.0 else 0.0
+    return kolm_seeing*factor
+
+
+def _seeingResid(r0_500, wavelength, L0, targetSeeing):
+    return _vkSeeing(r0_500, wavelength, L0) - targetSeeing
+
+
+def _r0_500(wavelength, L0, targetSeeing):
+    """Returns r0_500 to use to get target seeing."""
+    r0_500_max = min(1.0, L0*(1./2.183)**(-0.356)*(wavelength/500.)**1.2)
+    r0_500_min = 0.01
+    return bisect(
+        _seeingResid,
+        r0_500_min,
+        r0_500_max,
+        args=(wavelength, L0, targetSeeing)
+    )
+
+
+def makeAtmosphere(
+    airmass,
+    rawSeeing,
+    wavelength,
+    rng,
+    kcrit=0.2,
+    screen_size=819.2,
+    screen_scale=0.1,
+    nproc=6
+):
+    targetFWHM = (
+        rawSeeing/galsim.arcsec *
+        airmass**0.6 *
+        (wavelength/500.0)**(-0.3)
+    )
+
+    ud = galsim.UniformDeviate(rng)
+    gd = galsim.GaussianDeviate(rng)
+
+    # Use values measured from Ellerbroek 2008.
+    altitudes = [0.0, 2.58, 5.16, 7.73, 12.89, 15.46]
+    # Elevate the ground layer though.  Otherwise, PSFs come out too correlated
+    # across the field of view.
+    altitudes[0] = 0.2
+
+    # Use weights from Ellerbroek too, but add some random perturbations.
+    weights = [0.652, 0.172, 0.055, 0.025, 0.074, 0.022]
+    weights = [np.abs(w*(1.0 + 0.1*gd())) for w in weights]
+    weights = np.clip(weights, 0.01, 0.8)  # keep weights from straying too far.
+    weights /= np.sum(weights)  # renormalize
+
+    # Draw outer scale from truncated log normal
+    L0 = 0
+    while L0 < 10.0 or L0 > 100:
+        L0 = np.exp(gd() * 0.6 + np.log(25.0))
+    # Given the desired targetFWHM and randomly selected L0, determine
+    # appropriate r0_500
+    r0_500 = _r0_500(wavelength, L0, targetFWHM)
+
+    # Broadcast common outer scale across all layers
+    L0 = [L0]*6
+
+    # Uniformly draw layer speeds between 0 and max_speed.
+    maxSpeed = 20.0
+    speeds = [ud()*maxSpeed for _ in range(6)]
+    # Isotropically draw directions.
+    directions = [ud()*360.0*galsim.degrees for _ in range(6)]
+
+    atmKwargs = dict(
+        r0_500=r0_500,
+        L0=L0,
+        speed=speeds,
+        direction=directions,
+        altitude=altitudes,
+        r0_weights=weights,
+        rng=rng,
+        screen_size=screen_size,
+        screen_scale=screen_scale
+    )
+
+    ctx = multiprocessing.get_context('fork')
+    atm = galsim.Atmosphere(mp_context=ctx, **atmKwargs)
+
+    r0_500 = atm.r0_500_effective
+    r0 = r0_500 * (wavelength/500.0)**(6./5)
+    kmax = kcrit/r0
+
+    with ctx.Pool(
+        nproc,
+        initializer=galsim.phase_screens.initWorker,
+        initargs=galsim.phase_screens.initWorkerArgs()
+    ) as pool:
+        atm.instantiate(pool=pool, kmax=kmax, check='phot')
+
+    return atm
+
+
+class StarSimulator:
+    def __init__(
+        self,
+        observation,  # from OpSim
+        atmSettings,  # Atmospheric screen settings
+        telescope,  # batoid.Optic
+        rng=None,
+    ):
+        if rng is None:
+            rng = galsim.BaseDeviate()
+        self.observation = observation
+
+        self.wavelength = wavelength_dict[observation['band']]
+        self.atm = makeAtmosphere(
+            observation['airmass'],
+            observation['rawSeeing'],
+            self.wavelength,
+            rng,
+            kcrit=atmSettings['kcrit'],
+            screen_size=atmSettings['screen_size'],
+            screen_scale=atmSettings['screen_scale'],
+            nproc=atmSettings['nproc']
+        )
+
+        # and pre-cache a 2nd kick
+        psf = self.atm.makePSF(self.wavelength, diam=8.36)
+        _ = psf.drawImage(nx=1, ny=1, n_photons=1, rng=rng, method='phot')
+        self.second_kick = psf.second_kick
+
+        self.bandpass = galsim.Bandpass(
+            f"LSST_{observation['band']}.dat", wave_type='nm'
+        )
+
+        self.telescope = telescope
+
+        # Develop gnomonic projection from ra/dec to field angle using
+        # GalSim TanWCS class.
+        q = observation['rotTelPos'] - observation['rotSkyPos']
+        cq, sq = np.cos(q), np.sin(q)
+        affine = galsim.AffineTransform(cq, -sq, sq, cq)
+        self.radecToField = galsim.TanWCS(
+            affine,
+            self.observation['boresight'],
+            units=galsim.radians
+        )
+
+
+    def simStar(self, coord, sed, nphoton, rng, return_photons=False):
+        fieldAngle = self.radecToField.toImage(coord)
+        # Populate pupil
+        r_outer = 8.36/2
+        # purposely underestimate inner radius a bit.
+        # Rays that miss will be marked vignetted.
+        r_inner = 8.36/2*0.58
+        ud = galsim.UniformDeviate(rng)
+        r = np.empty(nphoton)
+        ud.generate(r)
+        r *= (r_outer**2 - r_inner**2)
+        r += r_inner**2
+        r = np.sqrt(r)
+
+        th = np.empty(nphoton)
+        ud.generate(th)
+        th *= 2*np.pi
+        u = r*np.cos(th)
+        v = r*np.sin(th)
+
+        # import matplotlib.pyplot as plt
+        # plt.scatter(u, v, s=0.1, alpha=0.1)
+        # plt.show()
+
+        # uniformly distribute photon times throughout 30s exposure
+        t = np.empty(nphoton)
+        ud.generate(t)
+        t *= self.observation['exptime']
+
+        # evaluate phase gradients at appropriate location/time
+        dku, dkv = self.atm.wavefront_gradient(
+            u, v, t, (fieldAngle.x*galsim.radians, fieldAngle.y*galsim.radians)
+        )  # output is in nm per m.  convert to radians
+        dku *= 1.e-9
+        dkv *= 1.e-9
+
+        # import matplotlib.pyplot as plt
+        # plt.scatter(dku*206265, dkv*206265, s=0.1, alpha=0.1)
+        # plt.show()
+
+        # add in second kick
+        pa = galsim.PhotonArray(nphoton)
+        self.second_kick._shoot(pa, rng)
+        dku += pa.x*(galsim.arcsec/galsim.radians)
+        dkv += pa.y*(galsim.arcsec/galsim.radians)
+
+        # import matplotlib.pyplot as plt
+        # plt.scatter(pa.x, pa.y, s=0.1, alpha=0.1)
+        # plt.scatter(dku*206265, dkv*206265, s=0.1, alpha=0.1)
+        # plt.show()
+
+        # assign wavelengths.
+        wavelengths = sed.sampleWavelength(nphoton, self.bandpass, rng)
+
+        # Chromatic seeing.  Scale deflections by (lam/500)**(-0.3)
+        dku *= (wavelengths/500)**(-0.3)
+        dkv *= (wavelengths/500)**(-0.3)
+
+        # DCR.  dkv is aligned along meridian, so only need to shift in this
+        # direction (I think)
+        base_refraction = galsim.dcr.get_refraction(
+            self.wavelength,
+            self.observation['zenith'],
+            temperature=self.observation['temperature'],
+            pressure=self.observation['pressure'],
+            H2O_pressure=self.observation['H2O_pressure'],
+        )
+        refraction = galsim.dcr.get_refraction(
+            wavelengths,
+            self.observation['zenith'],
+            temperature=self.observation['temperature'],
+            pressure=self.observation['pressure'],
+            H2O_pressure=self.observation['H2O_pressure'],
+        )
+        refraction -= base_refraction
+        dkv += refraction
+
+        # We're through the atmosphere!  Make a structure that batoid can use
+        # now.  Note we're going to just do the sum in the tangent plane
+        # coordinates.  This isn't perfect, but almost certainly good enough to
+        # still be interesting.
+        dku += fieldAngle.x
+        dkv += fieldAngle.y
+        vx, vy, vz = batoid.utils.fieldToDirCos(dku, dkv, projection='gnomonic')
+
+        # Place rays on entrance pupil - the planar cap coincident with the rim
+        # of M1.  Eventually may want to back rays up further so that they can
+        # be obstructed by struts, e.g..
+        x = u
+        y = v
+        zPupil = self.telescope["M1"].surface.sag(0, 0.5*self.telescope.pupilSize)
+        z = np.zeros_like(x)+zPupil
+        # TODO: really need to rescale velocities so that they're consistent
+        # with refractive index of air.  But medium.getN() isn't currently
+        # parallelized in batoid.  Too slow for a for-loop in python, so for
+        # now, just do the correction the effective wavelength.
+        n = self.telescope.inMedium.getN(self.wavelength)
+        vx /= n
+        vy /= n
+        vz /= n
+        rays = batoid.RayVector.fromArrays(
+            x, y, z, vx, vy, vz, t=np.zeros_like(x), w=wavelengths*1e-9, flux=1
+        )
+
+        self.telescope.traceInPlace(rays)
+
+        # Need to convert to pixels for galsim sensor object
+        # Put batoid results back into photons
+        # Use the same array.
+        pa.x = rays.x/10e-6
+        pa.y = rays.y/10e-6
+        pa.dxdz = rays.vx/rays.vz
+        pa.dydz = rays.vy/rays.vz
+        pa.wavelength = wavelengths
+        pa.flux = ~rays.vignetted
+
+        # sensor = galsim.Sensor()
+        sensor = galsim.SiliconSensor()
+        image = galsim.Image(256, 256)  # hard code for now
+        image.setCenter(
+            int(np.mean(pa.x[~rays.vignetted])),
+            int(np.mean(pa.y[~rays.vignetted]))
+        )
+        sensor.accumulate(pa, image)
+
+        if return_photons:
+            return image, pa
+        else:
+            return image
+
+
+if __name__ == '__main__':
+    # Just make up something from OpSim database
+    # It's entirely possible that this combination is *impossible* to achieve
+    # from Cerro Pachon...
+    observation = {
+        'boresight': galsim.CelestialCoord(
+            30*galsim.degrees, 10*galsim.degrees
+        ),
+        'zenith': 30*galsim.degrees,
+        'airmass': 1.1547,
+        'rotTelPos': 35*galsim.degrees,  # zenith measured CCW from up
+        'rotSkyPos': 10*galsim.degrees,  # N measured CCW from up
+        'rawSeeing': 0.7*galsim.arcsec,
+        'band': 'i',
+        'exptime': 15.0,
+        'temperature': 293.15,  # K
+        'pressure': 69.328,  # kPa
+        'H2O_pressure': 1.067,  # kPa
+    }
+
+    atmSettings = {
+        'kcrit': 0.2,
+        # 'screen_size': 819.2,
+        'screen_size': 102.4,
+        'screen_scale': 0.1,
+        'nproc': 6,
+    }
+
+    rng = galsim.BaseDeviate(57721)
+
+    # Could put in chip-to-chip information here.  Omit for the moment.
+    # Put in defocus and rotation here.
+    factory = LSSTFactory(observation['band'])
+    visit_telescope = factory.make_visit_telescope(
+        M2_amplitude = 0.8,
+        camera_amplitude = 0.8,
+        M1M3_bend_amplitude = 0.8,
+        # M2_amplitude = 0.18,
+        # camera_amplitude = 0.18,
+        # M1M3_bend_amplitude = 0.18,
+        rng = rng,
+        rotation = observation['rotTelPos'].rad,
+        defocus = 1.5e-3
+    )
+    telescope = visit_telescope.actual_telescope
+
+    simulator = StarSimulator(
+        observation,
+        atmSettings,
+        telescope,
+        rng=rng,
+    )
+
+    ras = []
+    decs = []
+    Ts = []
+    for _ in range(100):
+        dist = 100*galsim.degrees
+        while dist > 1.9*galsim.degrees:
+            ra = np.random.uniform(28.0, 32.0)
+            dec = np.random.uniform(8.0, 12.0)
+            coord = galsim.CelestialCoord(
+                ra*galsim.degrees, dec*galsim.degrees
+            )
+            dist = observation['boresight'].distanceTo(coord)
+        ras.append(ra)
+        decs.append(dec)
+        Ts.append(np.random.uniform(4000, 10000))
+
+    import matplotlib.pyplot as plt
+    plt.axis()
+    plt.ion()
+    plt.show()
+    for ra, dec, T in zip(tqdm(ras), decs, Ts):
+        coord = galsim.CelestialCoord(
+            ra*galsim.degrees, dec*galsim.degrees
+        )
+        sed = BBSED(T)
+        nphoton = int(1e6)
+        starImage, starPhotons = simulator.simStar(
+            coord, sed, nphoton, rng, return_photons=True
+        )
+
+        plt.imshow(starImage.array)
+        plt.draw()
+        plt.pause(0.1)
+
+        field = simulator.radecToField.toImage(coord)
+        zernikes = visit_telescope.get_zernike(field.x, field.y, jmax=11)
+        for j in range(4, 12):
+            print(f"Z{j:<4}  {zernikes[j]:7.3f}")
--- wfTel.py
+++ wfTel.py
@@ -5,17 +5,49 @@ import numpy as np
 import batoid
 from galsim.utilities import lazy_property
 from galsim.zernike import zernikeRotMatrix
+from galsim import BaseDeviate, GaussianDeviate
 
+wavelength_dict = dict(
+    u=365.49,
+    g=480.03,
+    r=622.20,
+    i=754.06,
+    z=868.21,
+    y=991.66
+)
 
-effective_wavelengths = {
-    'u':350e-9,
-    'g':480e-9,
-    'r':620e-9,
-    'i':750e-9,
-    'z':880e-9,
-    'y':975e-9
+norm = {
+    "M2 x":            [-0.000166,   0.000166],  # meters
+    "M2 y":            [-0.000166,   0.000166],
+    "M2 z":            [-1.35e-05,   9.14e-06],
+    "M2 thx":          [-2.49e-05,   2.49e-05],  # radians
+    "M2 thy":          [-2.49e-05,   2.49e-05],
+    "camera x":        [-0.000817,   0.000817],  # meters
+    "camera y":        [-0.000817,   0.000817],
+    "camera z":        [-1.31e-05,    8.8e-06],
+    "camera thx":      [-5.92e-05,   5.92e-05],  # radians
+    "camera thy":      [-5.92e-05,   5.92e-05],
+    "M1M3 bend 1":     [  -0.0567,     0.0567],  # not sure?
+    "M1M3 bend 2":     [  -0.0568,     0.0567],
+    "M1M3 bend 3":     [  -0.0555,     0.0792],
+    "M1M3 bend 4":     [  -0.0608,     0.0607],
+    "M1M3 bend 5":     [    -0.06,       0.06],
+    "M1M3 bend 6":     [  -0.0575,     0.0575],
+    "M1M3 bend 7":     [  -0.0576,     0.0576],
+    "M1M3 bend 8":     [  -0.0636,     0.0636],
+    "M1M3 bend 9":     [  -0.0633,     0.0634],
+    "M1M3 bend 10":    [  -0.0605,     0.0605],
+    "M1M3 bend 11":    [  -0.0605,     0.0611],
+    "M1M3 bend 12":    [   -0.171,       0.12],
+    "M1M3 bend 13":    [  -0.0657,     0.0658],
+    "M1M3 bend 14":    [  -0.0659,     0.0659],
+    "M1M3 bend 15":    [   -0.101,      0.101],
+    "M1M3 bend 16":    [   -0.101,      0.101],
+    "M1M3 bend 17":    [  -0.0587,     0.0587],
+    "M1M3 bend 18":    [  -0.0598,     0.0598],
+    "M1M3 bend 19":    [  -0.0695,     0.0696],
+    "M1M3 bend 20":    [  -0.0696,     0.0699]
 }
-
 
 with open("mirrorModes.pkl", 'rb') as f:
     bendingModeDict = pickle.load(f)
@@ -83,7 +115,7 @@ class LSSTFactory:
         chips=None,
     ):
         self.band = band
-        self.wavelength = effective_wavelengths[band]
+        self.wavelength = wavelength_dict[band]*1e-9 # nm -> m
 
         self.M1_figure_coef = M1_figure_coef
         self.M2_figure_coef = M2_figure_coef
@@ -148,8 +180,10 @@ class LSSTFactory:
         camera_tilt = (0,0),
         M1M3_bend = None,
         M2_bend = None,
+        defocus = 0.0,
         rotation = 0.0,
-        dof = None
+        dof = None,
+        **kwargs
     ):
         """Create a perturbed telescope for a single visit.
         """
@@ -164,10 +198,15 @@ class LSSTFactory:
 
         return VisitTelescope(
             self,
-            M2_shift, M2_tilt,
-            camera_shift, camera_tilt,
-            M1M3_bend, M2_bend,
-            rotation
+            M2_shift=M2_shift,
+            M2_tilt=M2_tilt,
+            camera_shift=camera_shift,
+            camera_tilt=camera_tilt,
+            M1M3_bend=M1M3_bend,
+            M2_bend=M2_bend,
+            defocus=defocus,
+            rotation=rotation,
+            **kwargs
         )
 
 
@@ -180,10 +219,15 @@ class VisitTelescope:
         Factory with permanent errors set.
     M2_shift : 3-tuple; meters
     M2_tilt : 2-tuple; radians
+    M2_amplitude : float; sets M2_shift and M2_tilt randomly
     camera_shift : 3-tuple; meters
     camera_tilt : 2-tuple; radians
+    camera_amplitude : float; sets M2_shift and M2_tilt randomly
     M1M3_bend : 20-tuple
+    M1M3_bend_amplitude : float; sets M1M3_bend randomly
     M2_bend : 20-tuple
+    M2_bend_amplitude : float; sets M2_bend randomly
+    defocus : float; meters
     rotation : float
         Camera rotation in radians.
     """
@@ -192,12 +236,84 @@ class VisitTelescope:
         factory,
         M2_shift=(0,0,0),
         M2_tilt=(0,0),
+        M2_amplitude=None,
         camera_shift=(0,0,0),
         camera_tilt=(0,0),
+        camera_amplitude=None,
         M1M3_bend=None,
+        M1M3_bend_amplitude=None,
         M2_bend=None,
-        rotation=0
+        M2_bend_amplitude=None,
+        defocus=0.0,
+        rotation=0,
+        rng=None
     ):
+        if rng is None:
+            rng = BaseDeviate()
+        gd = GaussianDeviate(rng)
+
+        if M2_amplitude is not None:
+            M2_dx = gd()
+            M2_dx *= np.ptp(norm['M2 x'])/2
+            M2_dx += np.mean(norm['M2 x'])
+            M2_dy = gd()
+            M2_dy *= np.ptp(norm['M2 y'])/2
+            M2_dy += np.mean(norm['M2 y'])
+            M2_dz = gd()
+            M2_dz *= np.ptp(norm['M2 z'])/2
+            M2_dz += np.mean(norm['M2 z'])
+            M2_shift = np.array([M2_dx, M2_dy, M2_dz])
+            M2_shift *= M2_amplitude
+
+            M2_thx = gd()
+            M2_thx *= np.ptp(norm['M2 thx'])/2
+            M2_thx += np.mean(norm['M2 thx'])
+            M2_thy = gd()
+            M2_thy *= np.ptp(norm['M2 thy'])/2
+            M2_thy += np.mean(norm['M2 thy'])
+            M2_tilt = np.array([M2_thx, M2_thy])
+            M2_tilt *= M2_amplitude
+
+        if camera_amplitude is not None:
+            camera_dx = gd()
+            camera_dx *= np.ptp(norm['camera x'])/2
+            camera_dx += np.mean(norm['camera x'])
+            camera_dy = gd()
+            camera_dy *= np.ptp(norm['camera y'])/2
+            camera_dy += np.mean(norm['camera y'])
+            camera_dz = gd()
+            camera_dz *= np.ptp(norm['camera z'])/2
+            camera_dz += np.mean(norm['camera z'])
+            camera_shift = np.array([camera_dx, camera_dy, camera_dz])
+            camera_shift *= camera_amplitude
+
+            camera_thx = gd()
+            camera_thx *= np.ptp(norm['camera thx'])/2
+            camera_thx += np.mean(norm['camera thx'])
+            camera_thy = gd()
+            camera_thy *= np.ptp(norm['camera thy'])/2
+            camera_thy += np.mean(norm['camera thy'])
+            camera_tilt = np.array([camera_thx, camera_thy])
+            camera_tilt *= camera_amplitude
+
+        if M1M3_bend_amplitude is not None:
+            M1M3_bend = []
+            for i in range(1, 21):
+                val = gd()
+                val *= np.ptp(norm[f'M1M3 bend {i}'])/2
+                val += np.mean(norm[f'M1M3 bend {i}'])
+                M1M3_bend.append(val)
+            M1M3_bend = np.array(M1M3_bend)*M1M3_bend_amplitude
+
+        if M2_bend_amplitude is not None:
+            M2_bend = []
+            for i in range(1, 21):
+                val = gd()
+                val *= np.ptp(norm[f'M2 bend {i}'])/2
+                val += np.mean(norm[f'M2 bend {i}'])
+                M2_bend.append(val)
+            M2_bend = np.array(M2_bend)*M2_bend_amplitude
+
         self.factory = factory
         self.M2_shift = M2_shift
         self.M2_tilt = M2_tilt
@@ -206,6 +322,7 @@ class VisitTelescope:
         self.M1M3_bend = M1M3_bend
         self.M2_bend = M2_bend
         self.rotation = rotation
+        self.defocus = defocus
 
         if self.M1M3_bend is not None:
             self.M1_grid = np.einsum(
@@ -222,7 +339,7 @@ class VisitTelescope:
 
     @lazy_property
     def fiducial_telescope(self):
-        """Unperturbed, unrotated, no chip gaps"""
+        """Unperturbed, unrotated, undefocused, no chip gaps"""
         return self.factory.fiducial_telescope
 
     @lazy_property
@@ -235,7 +352,7 @@ class VisitTelescope:
 
     @lazy_property
     def actual_telescope(self):
-        """Perturbed and rotated.  No chip gaps."""
+        """Perturbed, rotated, defocused.  No chip gaps."""
         telescope = self.fiducial_telescope
 
         M1_terms = [telescope['M1'].surface]
@@ -288,6 +405,7 @@ class VisitTelescope:
             .withGloballyShiftedOptic('LSSTCamera', self.camera_shift)
             .withLocallyRotatedOptic('LSSTCamera', rot(*self.camera_tilt))
             .withLocallyRotatedOptic('LSSTCamera', batoid.RotZ(self.rotation))
+            .withGloballyShiftedOptic('LSSTCamera', (0, 0, self.defocus))
         )
         return telescope
 
@@ -335,6 +453,8 @@ class VisitTelescope:
 
     def get_chip_telescope(self, thx, thy):
         """Return perturbed, rotated, gappy telescope for chip at thx, thy"""
+        if self.factory.chips is None:
+            return self.actual_telescope
         k = self.get_chip(thx, thy)
         return self.chip_telescopes[k]
 
